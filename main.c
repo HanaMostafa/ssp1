922d8e02956f204cf77e268412f2c557a08b24db
hanamostafa <hanamostafakamal9@gmail.com>
2021-10-26 15:14:58 +0200

    second

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"ssp.h"
-void getdata(uint8 * data,uint16 z){
+void getdata(uint8 * data,uint16 *z){
 	uint8 dataflag=notready;
 	uint8 i;
 
 	uint8 arr[]={0x24,0xdb,0xc0,0xda,0x29,0xb4,0xc0,0x54,0xc0,0x33,0xc0,0xc0,0xc0,0x46,0xc0,0x37,0xc0,0x28,0xc0,0x37,0xc0,0x16,0x19,0x68,0xc0,0xdb};
-(z)=sizeof(arr);
+*z=sizeof(arr);
 
 //z=*(&arr + 1) - arr;
 
-	for(i=0;i<z;i++){
+	for(i=0;i<*z;i++){
     data[i]=arr[i];
 
 }
@@ -21,7 +21,7 @@ int main()
 {
 uint8 dataflag=0;
 uint8 size=0;
-uint16 z=26;
+uint16 z;
 uint8 i;
 uint8 desti=0x9a;
 uint8 srce=0x6b;
@@ -32,11 +32,8 @@ uint8 data[info];
 uint8 rxframe[dt];
 //uint16 z=sizeof(arr);
 
-getdata(&data,z);
-printf("z= %d\n",z);
-for(i=0;i<z;i++){
-   printf("data %x\n",data[i]);
-}
+getdata(&data,&z);
+
 
 uint8 adddest;
 uint8 addsrc;
